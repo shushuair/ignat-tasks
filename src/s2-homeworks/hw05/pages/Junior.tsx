74ce1710a0c434ce5f431c92b3885cd1a91452ed
shushuair <shushulkov1989@gmail.com>
2023-06-21 09:11:26 +0100

    HW 8 add

--- src/s2-homeworks/hw05/pages/Junior.tsx
+++ src/s2-homeworks/hw05/pages/Junior.tsx
@@ -10,7 +10,7 @@ function Junior() {
         <div id={'hw5-page-junior'}>
             <HW6/>
             <HW7/>
-            {/*<HW8/>*/}
+            <HW8/>
             {/*<HW9/>*/}
         </div>
     )
